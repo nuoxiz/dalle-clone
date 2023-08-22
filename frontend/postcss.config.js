839f03552ee5d2ff27b4411409014892575b37d1
Nuoxi Zhang <zhangnuoxi24@gmail.com>
2023-08-22 18:47:21 +0100

    changed postcss config

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,8 @@
+import autoprefixer from "autoprefixer"
+import tailwindcss from "tailwindcss"
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: tailwindcss,
+    autoprefixer: autoprefixer,
   },
-}
+};
